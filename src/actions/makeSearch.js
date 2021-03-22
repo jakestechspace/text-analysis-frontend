70df66ad00d55c86ff311bc52ee346b0d5d946cc
JakesTechSpace <techlead@jakestechspace.com>
2021-03-22 12:05:31 -0400

    add make search function

---
+++ src/actions/makeSearch.js
@@ -0,0 +1,12 @@
+export default async function makeSearch(text) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text })
+  }
+  return fetch('https://text-analysis-backend.herokuapp.com/sentiment', options)
+    .then(response => response.json())
+    .then(data => data);  
+}
