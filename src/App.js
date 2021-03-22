0f27dcd2fb4db04ca8e43ec2bb03ed276b40f134
JakesTechSpace <techlead@jakestechspace.com>
2021-03-22 12:05:19 -0400

    updating templates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import makeSearch from './actions/makeSearch';
+import Form from './containers/form';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => props.makeSearch("yayyy you are the best")}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form />
       </header>
     </div>
   );
+}
+
+App.defaultProps = {
+  makeSearch
 }
 
 export default App;
