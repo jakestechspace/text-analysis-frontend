024f42fc7f31accac7f5f05e47092e510a0400bf
JakesTechSpace <techlead@jakestechspace.com>
2021-03-22 19:44:35 -0400

    update text area size

--- src/components/formInput.js
+++ src/components/formInput.js
@@ -2,8 +2,8 @@ export const FormInput = ({ setText }) => {
   return (
     <textarea
       onChange={(e) => setText(e.target.value)}
-      rows="5"
-      cols="33"
+      rows="8"
+      cols="75"
     />
   )
 }
