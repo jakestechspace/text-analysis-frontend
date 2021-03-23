33a20061497353e984741dad1d668d987dd9e190
JakesTechSpace <techlead@jakestechspace.com>
2021-03-23 09:07:23 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import './App.css'
+import './App.scss'
 import Form from './containers/form'
 import { useState } from 'react'
 import ResultContainer from './containers/resultContainer'
 
-export default function App(props) {
-  // initial state for result which comes back from analytics api
+export default function App() {
+  // initial state for api result
   const [result, setResult] = useState(null)
-  console.log('result', result)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Form setResult={setResult} />
-        {result && <ResultContainer result={result} />}
-      </header>
+    <div className="container">
+      {!result && <Form setResult={setResult} />}
+      {result && (
+        <>
+          <span
+            onClick={() => setResult(null)}
+            className="clear-results"
+          >
+            (x) clear
+          </span>
+          <div className="result-holder">
+            <ResultContainer result={result} />
+          </div>
+        </>
+      )}
     </div>
   )
 }
