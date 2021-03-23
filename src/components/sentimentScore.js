ebe2339121557b19860ac76ad0cb779377eec51b
JakesTechSpace <techlead@jakestechspace.com>
2021-03-23 19:01:00 -0400

    checking max score

--- src/components/sentimentScore.js
+++ src/components/sentimentScore.js
@@ -1,11 +1,19 @@
 export const SentimentScore = ({ confidenceScores }) => {
   const getEntries = Object.entries(confidenceScores);
 
+  // array of scores, ex: [.56, .32, .11]
+  const getValues = Object.values(confidenceScores)
+
   return (
     <div className="score-container">
       {getEntries.map(([key, val], i) => {
+        // check if the current val in loop is the highest
+        const isMax = Math.max(...getValues) === val
         return (
-          <div key={`score-${i}`} className="score">
+          <div
+            key={`score-${i}`}
+            className={isMax ? "score max" : "score"}
+          >
             <p>{val}</p>
             <span>{key}</span>
           </div>
