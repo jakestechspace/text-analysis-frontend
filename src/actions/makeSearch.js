d4fc45d337571d9a87f2571f74b332a6a4b07acf
JakesTechSpace <techlead@jakestechspace.com>
2021-03-24 07:06:44 -0400

    fix loading issue

--- src/actions/makeSearch.js
+++ src/actions/makeSearch.js
@@ -1,4 +1,7 @@
-export default async function makeSearch(text) {
+export default async function makeSearch(text, setLoading) {
+  // loading on
+  setLoading(true)
+
   const options = {
     method: "POST",
     headers: {
@@ -6,7 +9,13 @@ export default async function makeSearch(text) {
     },
     body: JSON.stringify({ text })
   }
+
+  // here is the fetch request that returns data from the back end
   return fetch('https://text-analysis-backend.herokuapp.com/sentiment', options)
     .then(response => response.json())
-    .then(data => data)
+    .then(data => {
+      // loading off
+      setLoading(false)
+      return data
+    })
 }
