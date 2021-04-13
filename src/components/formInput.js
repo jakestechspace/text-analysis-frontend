580308435d96de236003fe063f23a931ccee285f
JakesTechSpace <techlead@jakestechspace.com>
2021-04-12 23:00:18 -0400

    destructure sanitize function

--- src/components/formInput.js
+++ src/components/formInput.js
@@ -1,9 +1,9 @@
-import DOMPurify from 'dompurify';
+import { sanitize } from 'dompurify';
 
 export const FormInput = ({ setText }) => {
   return (
     <textarea
-      onChange={(e) => setText(DOMPurify.sanitize(e.target.value))}
+      onChange={(e) => setText(sanitize(e.target.value))}
       rows="8"
     />
   )
